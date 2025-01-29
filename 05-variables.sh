ce6264403c8cacc2a1fd43efc96856562f2d5576
padmamuvva <info@joindevops.com>
2025-01-29 15:53:08 +0530

    Conversation changes

---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Please enter your username::" #text entered here will be added as value to variable
+
+read USERNAME #here USERNME is the variable name
+
+echo "username entered: $USERNAME"
