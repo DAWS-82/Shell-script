723d3980a5c27812122880beb339e731bd1728c6
padmamuvva <info@joindevops.com>
2025-01-29 15:39:10 +0530

    Conversation changes

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,4 +6,4 @@ PERSON2=$2
 echo "${PERSON1}:: Hi $PERSON2 "
 echo "$PERSON2:: Hi $PERSON1"
 echo "$PERSON1:: How are you?"
-echo "$PERSON2:: I am fine now.How are you
+echo "$PERSON2:: I am fine now.How are you?'
