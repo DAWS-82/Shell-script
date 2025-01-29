e76324cd1d029954a6683ea176e6321509d137f4
padmamuvva <info@joindevops.com>
2025-01-29 15:31:55 +0530

    Conversation changes

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Ramesh  #no space before and after equal
+PERSON2=Suresh
+
+echo "$PERSON1:: Hi $PERSON2 "
+echo "$PERSON2:: Hi $PERSON1"
+echo "$PERSON1:: How are you?"
+echo "$PERSON2:: I am fine now.How are you?"
