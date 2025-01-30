4ac730213986d8e81bac2345fc41a2edc441f9d0
padmamuvva <info@joindevops.com>
2025-01-30 15:51:30 +0530

    special variables script added

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -6,6 +6,6 @@ echo "script name: $0"
 echo "Present working directory: $PWD"
 echo "Home directory or current use: $HOME"
 echo "Which user is running this script: $USER"
-echo "Process id of current script: $$
+echo "Process id of current script: $$"
 sleep 60 &
 echo "Process id of last command in background: $!"
