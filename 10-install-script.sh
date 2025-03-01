f6c115861aa975e36a7971a0f61fde574e561ddd
padmamuvva <info@joindevops.com>
2025-03-01 08:06:20 +0530

    install mysql

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -8,23 +8,57 @@ then
    exit 1 #other than 0
 fi
 
-dnf install mysql -y
+dnf list installed mysql
+
+if [ $? -ne 0 ] #not installed
+then
+   dnf install mysql -y
+   if [ $? -ne 0 ]
+   then
+      echo "Installing MYSQL...FAILURE"
+      exit 1
+   else
+      echo "Installing MYSQL...SUCCESS"
+   fi
+else
+   echo "MYSQL is alreay...INSTALLED"
+fi
+
+
+# dnf install mysql -y
+
+# if [ $? -ne 0 ]
+# then
+#    echo "Installing MYSQL...FAILURE"
+#    exit 1
+# else
+#    echo "Installing MYSQL...SUCCESS"
+# fi
+
+dnf list installed git
 
 if [ $? -ne 0 ]
 then
-   echo "Installing MYSQL...FAILURE"
-   exit 1
+   dnf install git -y
+   if [ $? -ne 0 ]
+   then
+      echo "Installing Git...FAILURE"
+      exit 1
+   else
+      echo "Installing Git...SUCCESS"
+   fi
 else
-   echo "Installing MYSQL...SUCCESS"
+   echo "Git is already...INSTALLED"
 fi
 
-dnf install git -y
 
-if [ $? -ne 0 ]
-then
-   echo "Installing Git...FAILURE"
-   exit 1
-else
-   echo "Installing Git...SUCCESS"
-fi
+# dnf install git -y
+
+# if [ $? -ne 0 ]
+# then
+#    echo "Installing Git...FAILURE"
+#    exit 1
+# else
+#    echo "Installing Git...SUCCESS"
+# fi
   
