2d09b6a8d94ed68cb4ec2d0835c56ae285e46b7a
padmamuvva <info@joindevops.com>
2025-03-01 07:31:03 +0530

    install mysql

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -13,6 +13,7 @@ dnf install mysqll -y
 if [ $? -ne 0 ]
 then
    echo "Installing MYSQL...FAILURE"
+   exit 1
 else
    echo "Installing MYSQL...SUCCESS"
 fi
@@ -22,6 +23,7 @@ dnf install git -y
 if [ $? -ne 0 ]
 then
    echo "Installing Git...FAILURE"
+   exit 1
 else
    echo "Installing Git...SUCCESS"
 fi
