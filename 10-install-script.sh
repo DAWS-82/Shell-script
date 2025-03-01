99923007a231ed050d072216baaf552b7173897c
padmamuvva <info@joindevops.com>
2025-03-01 07:05:28 +0530

    install mysql

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-   echo " Error:: you must have sudo access to execute this script"
+   echo " ERROR:: you must have sudo access to execute this script"
 fi
 
-dnf install mysql -y
+dnf install mysqll -y
